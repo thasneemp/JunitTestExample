9f7e22e234b3175e7188f810eaa387c35569d56e
muhammed <muhammed.thasneem@yahoo.com>
2017-10-04 18:33:11 +0530

    instrumentation

--- AndroidJunitTestExample/app/src/androidTest/java/com/muhammed/testing/ExampleInstrumentedTest.java
+++ AndroidJunitTestExample/app/src/androidTest/java/com/muhammed/testing/ExampleInstrumentedTest.java
@@ -1,13 +1,21 @@
 package com.muhammed.testing;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -15,12 +23,24 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.muhammed.testing", appContext.getPackageName());
+    private static final String STRING_TO_BE_TYPED = "Thasneem";
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void sayHello() {
+        onView(withId(R.id.editText)).perform(typeText(STRING_TO_BE_TYPED), closeSoftKeyboard());
+
+        onView(withText("Say hello!")).perform(click());
+
+        String expectedText = "Hello, " + STRING_TO_BE_TYPED + "!";
+
+        onView(withId(R.id.textView)).check(ViewAssertions.matches(withText(expectedText)));
+
     }
+
 }
